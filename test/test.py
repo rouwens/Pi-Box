e0ff7f03883869967f1d6e1ba73d4d2f09b55f30
rouwens <drouwens@gmail.com>
2021-05-23 12:11:50 +0200

    Bijgewerkt

--- test/test.py
+++ test/test.py
@@ -8,6 +8,7 @@ fout = open("out.txt", "wt")
 for line in fin:
 	#read replace the string and write to output file
 	fout.write(line.replace('localhost', test))
+
 #close input and output files
 fin.close()
 fout.close()

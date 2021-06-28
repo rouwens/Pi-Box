5adb9edf69a3870dae18ddb014f12065ebdf14de
rouwens <drouwens@gmail.com>
2021-06-28 19:18:00 +0200

    Bijgewerkt

--- scripts/install/jellyfin.bash
+++ scripts/install/jellyfin.bash
@@ -1,5 +1,5 @@
 apt install apt-transport-https -y
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | apt-key add -
-echo "deb [arch=$( dpkg --print-architecture ) trusted=yes] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+echo "deb [arch=$( dpkg --print-architecture ) trusted=yes] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" |  tee /etc/apt/sources.list.d/jellyfin.list
 apt update
 apt install jellyfin -u

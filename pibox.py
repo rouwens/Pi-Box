ec1458b72a8859fa601548bbce7b4aa98316cee2
rouwens <drouwens@gmail.com>
2021-06-28 21:14:04 +0200

    Bijgewerkt

--- pibox.py
+++ pibox.py
@@ -1384,6 +1384,7 @@ def vpn (ip):
 
         if choice == "y":
             os.system ("cd /opt/Pi-Box/scripts && wget https://github.com/Nyr/openvpn-install/blob/master/openvpn-install.sh && mv openvpn-install.sh openvpn.sh")
+            os.system ("chmod +x /opt/Pi-Box/scripts/openvpn.sh")
             os.system ("/opt/Pi-Box/scripts/openvpn.sh")
             clear()
             print ("The VPN server is installed.")
